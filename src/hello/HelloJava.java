37d54cc1ad5de1b33a60c28149ed8638f61ff050
taruyo <itaru.yos@gmail.com>
2025-05-10 10:47:50 +0900

    商品一覧ページ作成

--- src/hello/HelloJava.java
+++ src/hello/HelloJava.java
@@ -4,6 +4,13 @@ public class HelloJava {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Java!");
+
+		System.out.println("商品一覧ページを作成する");
+		System.out.println("HTML上にテーブルを作成する。カラムは以下");
+		System.out.println("商品名");
+		System.out.println("商品説明（冒頭30字だけ表示）");
+		System.out.println("価格");
+		System.out.println("画像");
 	}
 
 }
